cf6646dc850f534350d4c20ee58352fac4e151f0
Allison Dedrick <agdedrick@ucdavis.edu>
2018-01-24 14:34:21 -0500

    Started script with common functions and constants to collect useful ones in one place to run before other scripts.

---
+++ Code/Common_constants_and_functions.R
@@ -0,0 +1,49 @@
+#Useful functions and constants for clownfish work
+#Inspired by Michelle's helper functions script, some functions and constants I expect to use multiple times
+
+#################### Constants, indices, etc. ####################
+##### Vector and indices to reference sites easily in plots and filtering and such (can call things like site_vec[Cabatoan] or site_vec[1]))
+site_vec <- c("Cabatoan", "Caridad Cemetery", "Caridad Proper", "Elementary School", "Gabas", "Haina", "Hicgop South",
+              "Magbangon", "Palanas", "Poroc Rose", "Poroc San Flower", "San Agustin", "Sitio Baybayon", "Sitio Lonas", 
+              "Sitio Tugas", "Tamakin Dacot", "Visca", "Wangag")
+Cabatoan <- 1
+CaridadCemetery <- 2
+CaridadProper <- 3
+ElementarySchool <- 4
+Gabas <- 5
+Haina <- 6
+HicgopSouth <- 7
+Magbangon <- 8
+Palanas <- 9
+PorocRose <- 10
+PorocSanFlower <- 11
+SanAgustin <- 12
+SitioBaybayon <- 13
+SitioLonas <- 14
+SitioTugas <- 15
+TamakinDacot <- 16
+Visca <- 17
+Wangag <- 18
+
+#tagging and fin-clipping thresholds
+min_tag_size <- 3.5 #minimum size for fin-clip 
+min_clip_size <- 6.0 #minimum size for tagging 
+
+#################### Functions: ####################
+#function to make vector of strings for column names for something done each year (like columns for sampling each year or minimum distance sampled to each anem each year, etc.)
+makeYearlyColNames <- function(start.Year, end.Year, descriptor) { #start.Year is first year of sampling, end.Year is final year of sampling, descriptor is string to go before year in column name (like "min_dist_" if want columns like "min_dist_2012")
+  
+  if (start.Year > end.Year) {
+    out <- NULL
+  } 
+  else {
+    out <- as.vector(NA) #initalize output vector of column names
+    year <- start.Year 
+    
+    for (i in 1:(end.Year - start.Year + 1)) {
+      out[i] <- paste(descriptor, year, sep="") #create string like "min_dist_2012", where descriptor is something like "min_dist_" and year is the year sampled
+      year <- year + 1
+    }
+  }
+  return(out)
+}
